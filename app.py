26ae48d452c6aa09ba3dd813ec5a897492617d02
naoyoshi001 <naoyochism@icloud.com>
2025-08-20 14:52:28 +0900

    port500に戻す

--- app.py
+++ app.py
@@ -549,5 +549,5 @@ def delete_session_keys(*keys):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
